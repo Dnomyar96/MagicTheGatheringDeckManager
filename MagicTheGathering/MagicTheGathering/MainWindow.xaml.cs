9601fe4a8c65cb9d5137377dabf80f51337986d5
Raymond Benjamins <raymond.benjamins@telfort.nl>
2017-04-07 23:23:20 +0200

    Added UI Elements and functionality

--- MagicTheGathering/MagicTheGathering/MainWindow.xaml.cs
+++ MagicTheGathering/MagicTheGathering/MainWindow.xaml.cs
@@ -27,19 +27,6 @@ namespace MagicTheGathering
         {
             InitializeComponent();
 
-            var service = new CardService();
-            var result = service.Where(x => x.SetName, "Kaladesh").All();
-            if(result.IsSuccess)
-            {
-                var cards = result.Value;
-
-                LoadList(cards);
-            }
-            else
-            {
-                var exception = result.Exception;
-            }
-
             colorComboBox.Items.Add("All");
             colorComboBox.Items.Add("Black");
             colorComboBox.Items.Add("Blue");
@@ -54,14 +41,57 @@ namespace MagicTheGathering
             rarityComboBox.Items.Add("Rare");
             rarityComboBox.Items.Add("Mythic Rare");
             rarityComboBox.SelectedIndex = 0;
+
+            typeComboBox.Items.Add("All");
+            typeComboBox.Items.Add("Creature");
+            typeComboBox.Items.Add("Sorcery");
+            typeComboBox.Items.Add("Instant");
+            typeComboBox.Items.Add("Enchantment");
+            typeComboBox.Items.Add("Artifact");
+            typeComboBox.Items.Add("Land");
+            typeComboBox.Items.Add("Planeswalker");
+            typeComboBox.SelectedIndex = 0;
+
+            LoadContent();
         }
 
-        private void button_Click(object sender, RoutedEventArgs e)
+        private async void LoadContent()
+        {
+            var cards = new List<Card>();
+            var service = new CardService();
+            var done = false;
+            var count = 1;
+            while (!done)
+            {
+                ShowSpinner();
+                var result = await service.Where(x => x.SetName, "Kaladesh").Where(x => x.Page, count).AllAsync();
+                if (result.IsSuccess)
+                {
+                    HideSpinner();
+                    if (result.Value.Count > 0)
+                    {
+                        foreach (var card in result.Value)
+                        {
+                            cards.Add(card);
+                        }
+                    }
+                    else
+                    {
+                        done = true;
+                    }
+                }
+                count++;
+            }
+            LoadList(cards);
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
         {
             if (listBox.SelectedItem != null)
             {
                 var service = new CardService();
-                var result = service.Where(x => x.Name, listBox.SelectedItem).All();
+                ShowSpinner();
+                var result = await service.Where(x => x.Name, listBox.SelectedItem).AllAsync();
                 if (result.IsSuccess)
                 {
                     var cards = result.Value;
@@ -79,6 +109,7 @@ namespace MagicTheGathering
                                 "cars.php&psig=AFQjCNH6LtqsKAb8o8LgthDCPQ9zyUGO_A&ust=1491233283080580"));
                         }
                     }
+                    HideSpinner();
                 }
             }
         }
@@ -86,6 +117,24 @@ namespace MagicTheGathering
         private void sortButton_Click(object sender, RoutedEventArgs e)
         {
             Filter();
+        }
+
+        private void ShowSpinner()
+        {
+            spinner.Visibility = Visibility.Visible;
+            listBox.IsEnabled = false;
+            button.IsEnabled = false;
+            sortButton.IsEnabled = false;
+            filterGrid.IsEnabled = false;
+        }
+
+        private void HideSpinner()
+        {
+            spinner.Visibility = Visibility.Collapsed;
+            listBox.IsEnabled = true;
+            button.IsEnabled = true;
+            sortButton.IsEnabled = true;
+            filterGrid.IsEnabled = true;
         }
 
         private void LoadList(List<Card> cards)
@@ -97,53 +146,104 @@ namespace MagicTheGathering
             }
         }
 
-        private void Filter()
+        private async void Filter()
         {
+            var cards = new List<Card>();
             var service = new CardService();
-            var result = service.Where(x => x.SetName, "Kaladesh").All();
-            if (result.IsSuccess)
+            var done = false;
+            var count = 1;
+            while (!done)
             {
-                var cards = result.Value;
-
-                var color = colorComboBox.SelectedValue;
-                switch (color)
+                ShowSpinner();
+                var result = await service.Where(x => x.SetName, "Kaladesh").Where(x => x.Page, count).AllAsync();
+                if (result.IsSuccess)
                 {
-                    case "All":
-                        break;
-                    case "Black":
-                        cards = FilterColor("Black", cards);
-                        break;
-                    case "Blue":
-                        cards = FilterColor("Blue", cards);
-                        break;
-                    case "Green":
-                        cards = FilterColor("Green", cards);
-                        break;
-                    case "Red":
-                        cards = FilterColor("Red", cards);
-                        break;
-                    case "White":
-                        cards = FilterColor("White", cards);
-                        break;
+                    HideSpinner();
+                    if (result.Value.Count > 0)
+                    {
+                        foreach (var card in result.Value)
+                        {
+                            cards.Add(card);
+                        }
+                    }
+                    else
+                    {
+                        done = true;
+                    }
                 }
-
-                var rarity = rarityComboBox.SelectedValue;
-                switch(rarity)
-                {
-                    case "All":
-                        break;
-                    case "Common":
-                        break;
-                    case "Uncommon":
-                        break;
-                    case "Rare":
-                        break;
-                    case "Mythic Rare":
-                        break;
-                }
-
-                LoadList(cards);
+                count++;
             }
+
+            var color = colorComboBox.SelectedValue;
+            switch (color)
+            {
+                case "All":
+                    break;
+                case "Black":
+                    cards = FilterColor("Black", cards);
+                    break;
+                case "Blue":
+                    cards = FilterColor("Blue", cards);
+                    break;
+                case "Green":
+                    cards = FilterColor("Green", cards);
+                    break;
+                case "Red":
+                    cards = FilterColor("Red", cards);
+                    break;
+                case "White":
+                    cards = FilterColor("White", cards);
+                    break;
+            }
+
+            var rarity = rarityComboBox.SelectedValue;
+            switch (rarity)
+            {
+                case "All":
+                    break;
+                case "Common":
+                    cards = FilterRarity("Common", cards);
+                    break;
+                case "Uncommon":
+                    cards = FilterRarity("Uncommon", cards);
+                    break;
+                case "Rare":
+                    cards = FilterRarity("Rare", cards);
+                    break;
+                case "Mythic Rare":
+                    cards = FilterRarity("Mythic Rare", cards);
+                    break;
+            }
+
+            var type = typeComboBox.SelectedValue;
+            switch (type)
+            {
+                case "All":
+                    break;
+                case "Creature":
+                    cards = FilterType("Creature", cards);
+                    break;
+                case "Sorcery":
+                    cards = FilterType("Sorcery", cards);
+                    break;
+                case "Instant":
+                    cards = FilterType("Instant", cards);
+                    break;
+                case "Enchantment":
+                    cards = FilterType("Enchantment", cards);
+                    break;
+                case "Artifact":
+                    cards = FilterType("Artifact", cards);
+                    break;
+                case "Land":
+                    cards = FilterType("Land", cards);
+                    break;
+                case "Planeswalker":
+                    cards = FilterType("Planeswalker", cards);
+                    break;
+            }
+
+            LoadList(cards);
         }
 
         private List<Card> FilterColor(string color, List<Card> cards)
@@ -154,6 +254,38 @@ namespace MagicTheGathering
                 if (card.Colors != null)
                 {
                     if (card.Colors.Contains(color))
+                    {
+                        result.Add(card);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private List<Card> FilterRarity(string rarity, List<Card> cards)
+        {
+            var result = new List<Card>();
+            foreach(var card in cards)
+            {
+                if(card.Rarity != null)
+                {
+                    if(card.Rarity == rarity)
+                    {
+                        result.Add(card);
+                    }
+                }
+            }
+            return result;
+        }
+
+        private List<Card> FilterType(string type, List<Card> cards)
+        {
+            var result = new List<Card>();
+            foreach (var card in cards)
+            {
+                if (card.Types != null)
+                {
+                    if (card.Types.Contains(type))
                     {
                         result.Add(card);
                     }
